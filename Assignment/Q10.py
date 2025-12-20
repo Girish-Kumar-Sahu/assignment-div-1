101036584dae244bf955bce701e19fe66524b37b
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2025-12-20 21:24:23 +0530

    fixed Q10 abnormal spikes section

--- Assignment/Q10.py
+++ Assignment/Q10.py
@@ -1,7 +1,16 @@
-lst = list(map(int, input('enter tempratures in celcius (seprate them by space): ').split()))
-print(f'average temp is {sum(lst)/len(lst)}C')
-print(f'maximum temperatur was {max(lst)} and minimum was {min(lst)}')
-# using standard deviation from numpy to detect abnormal temperatures
 import numpy as np
+
+lst = list(map(int, input('enter tempratures in celcius (seprate them by space): ').split()))
 lst = np.array(lst)
-print(f'abnormal temperature spikes are {np.std(lst)}')
+mean = np.mean(lst)
+print(f'average temp is {mean}C')
+print(f'maximum temperature was {max(lst)} and minimum was {min(lst)}')
+
+# using standard deviation from numpy to detect abnormal temperatures
+# using formula ∣x−μ|>k×std
+
+std = lst.std()
+# use 2 as a thresold for more rare spikes i used 1 for more common spikes
+AT = lst[abs(lst - mean) > 1 * std]
+
+print(f'abnormal temperature spikes are {AT}')
